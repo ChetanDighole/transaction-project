b36d9caabc6e1d286b685640295670359eabe5bf
ChetanDighole <digholechetan@gmail.com>
2024-07-13 16:53:53 +0530

    bugs

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,4 @@
 exports.requiredData = {
-    mongoURI: "mongodb+srv://digholechetan:chetan123@cluster0.g7hwr4t.mongodb.net/",
+    mongoURI: "mongodb+srv://digholechetan:chetan123@cluster0.g7hwr4t.mongodb.net/transaction_project",
     JWT_SECRETE: "payTMBackend"
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from '../components/Users'
 
 
+
 function Dashboard() {
+
+    const [balance, setBalance] = useState("")
+
+    async function getBalance() {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            console.error("Token is not available");
+            return;
+        }
+
+        try {
+            const res = await axios.get('http://localhost:4000/api/v1/account/balance', {
+                headers: { Authorization: "Bearer " + token }
+            });
+            if (res.data && res.data.balance !== undefined) {
+                setBalance(res.data.balance);
+            } else {
+                console.error("Unexpected response structure:", res.data);
+            }
+        } catch (error) {
+            console.error("Error fetching balance:", error);
+        }
+    }
+
+
+    useEffect(() => {
+        getBalance()
+    }, [])
+
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10,000"} />
+            <Balance value={balance} />
             <Users />
         </div>
     </div>
