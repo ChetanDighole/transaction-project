bea989b382ae76795c8e44efacfbcb03adce00c1
ChetanDighole <digholechetan@gmail.com>
2024-07-13 17:08:15 +0530

    env

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,4 @@
 exports.requiredData = {
-    mongoURI: "mongodb+srv://digholechetan:chetan123@cluster0.g7hwr4t.mongodb.net/transaction_project",
-    JWT_SECRETE: "payTMBackend"
+    mongoURI: process.env.MONGO_URL,
+    JWT_SECRETE: process.env.JWT_SECRETE
 }
